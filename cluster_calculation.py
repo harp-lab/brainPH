33f19919cd57f7177b605bc34399c06f9357a346
Ahmedur Rahman Shovon <shovon.sylhet@gmail.com>
2022-12-16 18:58:06 -0600

    updated clustering

--- cluster_calculation.py
+++ cluster_calculation.py
@@ -1,11 +1,13 @@
 import json
 import math
 import os
+
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.cluster import KMeans
 from sklearn.metrics import silhouette_score
 from utils import timer
+from distance_calculation import get_mds
 
 
 def get_dataset(datafile):
@@ -67,12 +69,12 @@ def generate_kmeans_clusters(mx_645_mds_path,
         os.makedirs(output_directory)
     dataset_mx_645 = get_dataset(mx_645_mds_path)
     dataset_mx_1400 = get_dataset(mx_1400_mds_path)
-    dataset_std_645 = get_dataset(std_2500_mds_path)
+    dataset_std_2500 = get_dataset(std_2500_mds_path)
     n_clusters_645, labels_645 = get_labels_highest_score(dataset_mx_645)
     unique_labels_645 = np.unique(labels_645)
     n_clusters_1400, labels_1400 = get_labels_highest_score(dataset_mx_1400)
     unique_labels_1400 = np.unique(labels_1400)
-    n_clusters_2500, labels_2500 = get_labels_highest_score(dataset_std_645)
+    n_clusters_2500, labels_2500 = get_labels_highest_score(dataset_std_2500)
     unique_labels_2500 = np.unique(labels_2500)
 
     cluster_info = [
@@ -100,7 +102,7 @@ def generate_kmeans_clusters(mx_645_mds_path,
     image_name = f"{output_directory}/clusters_std2500_{distance}.png"
     if single_figure:
         image_name = f"{output_directory}/clusters_{distance}.png"
-        plot_subplot(labels_2500, unique_labels_2500, dataset_std_645,
+        plot_subplot(labels_2500, unique_labels_2500, dataset_std_2500,
                      title, 3)
         plt.suptitle(f"Clustering result with {distance}")
         plt.tight_layout()
@@ -108,7 +110,7 @@ def generate_kmeans_clusters(mx_645_mds_path,
         plt.close()
     else:
         plot_independent_figure(labels_2500, unique_labels_2500,
-                                dataset_std_645, title, image_name)
+                                dataset_std_2500, title, image_name)
     print(f"Generated {image_name}")
 
     with open(cluster_file, "w") as json_file:
@@ -126,6 +128,83 @@ def show_clustering_table(labels_645, labels_1400, labels_2500):
     print("")
 
 
+def generate_random_distance_dataset(n, seed_value):
+    # seed for reproducibility
+    np.random.seed(seed_value)
+    data = [[0 for j in range(n)] for i in range(n)]
+    for i in range(n):
+        for j in range(i):
+            random_number = np.random.randint(100)
+            data[i][j] = random_number
+            data[j][i] = random_number
+    return data
+    # return np.random.randint(1000, size=(n, n))
+
+
+def generate_random_kmeans_clusters(output_directory):
+    if not os.path.exists(output_directory):
+        os.makedirs(output_directory)
+    distance_matrix_mx_645 = generate_random_distance_dataset(316, 645)
+    distance_matrix_mx_1400 = generate_random_distance_dataset(316, 1400)
+    distance_matrix_std_2500 = generate_random_distance_dataset(316, 2500)
+    dataset_mx_645 = get_mds(distance_matrix_mx_645)
+    dataset_mx_1400 = get_mds(distance_matrix_mx_1400)
+    dataset_std_2500 = get_mds(distance_matrix_std_2500)
+    n_clusters_645, labels_645 = get_labels_highest_score(dataset_mx_645)
+    unique_labels_645 = np.unique(labels_645)
+    n_clusters_1400, labels_1400 = get_labels_highest_score(dataset_mx_1400)
+    unique_labels_1400 = np.unique(labels_1400)
+    n_clusters_2500, labels_2500 = get_labels_highest_score(dataset_std_2500)
+    unique_labels_2500 = np.unique(labels_2500)
+
+    cluster_info = [
+        n_clusters_2500, n_clusters_1400, n_clusters_645
+    ]
+    cluster_file = f"{output_directory}/clusters_random.json"
+    title = f'mx645 (random): {n_clusters_645} clusters'
+    image_name = f"{output_directory}/clusters_mx645_random.png"
+    plot_independent_figure(labels_645, unique_labels_645, dataset_mx_645,
+                            title, image_name)
+    print(f"Generated {image_name}")
+    title = f'mx1400 (random): {n_clusters_1400} clusters'
+    image_name = f"{output_directory}/clusters_mx1400_random.png"
+    plot_independent_figure(labels_1400, unique_labels_1400,
+                            dataset_mx_1400, title, image_name)
+    print(f"Generated {image_name}")
+    title = f'std2500 (random): {n_clusters_2500} clusters'
+    image_name = f"{output_directory}/clusters_std2500_random.png"
+    plot_independent_figure(labels_2500, unique_labels_2500,
+                            dataset_std_2500, title, image_name)
+    print(f"Generated {image_name}")
+
+    with open(cluster_file, "w") as json_file:
+        json.dump(cluster_info, json_file)
+    print(f"Generated {cluster_file}")
+
+    return cluster_info
+
+
+def get_cluster_subject_group(labels):
+    cluster = [[], []]
+    for i, label in enumerate(labels):
+        cluster[label].append(i + 1)
+    return cluster
+
+
+def show_matching_between_clusters(cluster_645, cluster_1400, cluster_2500):
+    cluster_group = {}
+    for i in range(len(cluster_645)):
+        for j in range(len(cluster_1400)):
+            for k in range(len(cluster_2500)):
+                x = set(cluster_645[i]).intersection(set(cluster_1400[j]))
+                total_matches = x.intersection(set(cluster_2500[k]))
+                group_id = f"{i}{j}{k}"
+                cluster_group[group_id] = total_matches
+    for i in cluster_group:
+        print(f"Cluster group: {i}: match: {cluster_group[i]}")
+        print(f"#match: {len(cluster_group[i])}")
+        print("")
+
 def get_subjects_cluster_id(mx_645_mds_path,
                             mx_1400_mds_path,
                             std_2500_mds_path):
@@ -135,7 +214,20 @@ def get_subjects_cluster_id(mx_645_mds_path,
     n_clusters_645, labels_645 = get_labels_highest_score(dataset_mx_645)
     n_clusters_1400, labels_1400 = get_labels_highest_score(dataset_mx_1400)
     n_clusters_2500, labels_2500 = get_labels_highest_score(dataset_std_645)
-    show_clustering_table(labels_645, labels_1400, labels_2500)
+    cluster_645 = get_cluster_subject_group(labels_645)
+    cluster_1400 = get_cluster_subject_group(labels_1400)
+    cluster_2500 = get_cluster_subject_group(labels_2500)
+    # show_matching_between_clusters(cluster_645, cluster_1400, cluster_2500)
+
+    ar = [cluster_645[0], cluster_645[1], cluster_1400[0], cluster_1400[1], cluster_2500[0], cluster_2500[1]]
+    matrix = [[0 for j in range(6)]for i in range(6)]
+    for i in range(len(ar)):
+        for j in range(len(ar)):
+            matrix[i][j] = len(set(ar[i]).intersection(set(ar[j])))
+    for i in range(len(matrix)):
+        for j in range(len(matrix)):
+            print(matrix[i][j], end=" ")
+        print("")
 
 
 @timer
@@ -153,6 +245,10 @@ def main():
                                                output_dir, distance="ws",
                                                single_figure=False)
     print(f"Number of clusters in 3 cohorts: {cluster_summary}")
+
+    # random_cluster_summary = generate_random_kmeans_clusters(output_dir)
+    # print(
+    #     f"Number of clusters in 3 cohorts (random): {random_cluster_summary}")
 
 
 if __name__ == "__main__":
